da78c02a6fefc4126e5d8215364088c35dffb987
sycanz <aidenchan0397@gmail.com>
2025-06-13 00:45:21 +0800

    changed from implicit to authorization code grant type

--- src/scripts/auth/auth-flow.js
+++ src/scripts/auth/auth-flow.js
@@ -9,7 +9,8 @@ export function getToken() {
             const authUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth")
             authUrl.searchParams.append("client_id", client_id);
             authUrl.searchParams.append("redirect_uri", callbackUrl);
-            authUrl.searchParams.append("response_type", "token");
+            authUrl.searchParams.append("response_type", "code");
+            authUrl.searchParams.append("access_type", "offline");
             authUrl.searchParams.append("scope", scope);
             authUrl.searchParams.append("state", state);
             authUrl.searchParams.append("include_granted_scopes", "true");
@@ -22,19 +23,29 @@ export function getToken() {
                 {
                     url: authUrl.toString(),
                     interactive: true,
-                })
-                .then((url) => {
-                    const fragment = new URLSearchParams(url.split("#")[1]);
-                    const accessToken = fragment.get("access_token");
+                }
+            )
+            .then((url) => {
+                console.log("Reponse URL: ", url);
+                const fragment = new URLSearchParams(url.split("?")[1]);
+                const responseState = fragment.get("state");
+                const code = fragment.get("code");
 
-                    if (!accessToken) {
-                        throw new Error("No access token found in redirect URL");
-                    }
+                console.log("Orig state:", state);
+                console.log("Response state", responseState);
 
-                    console.log("Access token: ", accessToken);
-                    resolve(accessToken)
-                })
-                .catch((err) => console.log(err));
+                if (state != responseState) {
+                    throw new Error("If you're not the actual user, GET OUT!")
+                }
+
+                if (!code) {
+                    throw new Error("No access token found in redirect URL");
+                }
+
+                console.log("Code: ", code);
+                // resolve(accessToken)
+            })
+            .catch((err) => console.log(err));
         } catch (error) {
             reject(new Error(`Sign-in failed: ${error.message}`));
         }
