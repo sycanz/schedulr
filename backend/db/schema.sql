1db87491590a45c23d526dc149668b6fcf8fcd15
sycanz <aidenchan0397@gmail.com>
2026-04-01 01:23:50 +0800

    refactor: tidy up db schemas

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -3,18 +3,18 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- users table
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    email TEXT,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4()
+    email TEXT NOT NULL UNIQUE,
     name TEXT,
-    sub TEXT UNIQUE,
+    sub TEXT NOT NULL UNIQUE,
     created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
 );
 
 -- oauth_tokens table
 CREATE TABLE oauth_tokens (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE UNIQUE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE UNIQUE,
     access_token TEXT,
     refresh_token TEXT,
     access_token_expires_at TIMESTAMPTZ,
@@ -26,11 +26,11 @@ CREATE TABLE oauth_tokens (
 -- sessions table
 CREATE TABLE sessions (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE UNIQUE,
-    session_token UUID UNIQUE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE UNIQUE,
     expires_at TIMESTAMPTZ,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
     last_accessed_at TIMESTAMPTZ DEFAULT NOW(),
-    ip_address TEXT,
     user_agent TEXT,
-    created_at TIMESTAMPTZ DEFAULT NOW()
+    ip_address TEXT,
+    session_token UUID NOT NULL UNIQUE
 );
